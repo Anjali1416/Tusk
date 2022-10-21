ce0975a859b2e84c943698ed9052100301ca0de1
Anjali1416 <42797403+Anjali1416@users.noreply.github.com>
2022-10-21 12:17:13 +0530

    java file added

---
+++ CollectionConversion.java
@@ -0,0 +1,36 @@
+package Classes;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CollectionConversion {
+
+	public static void main(String[] args) {
+		int [] arr1= {1,4,6,6,3};
+		List<Integer> al=new ArrayList<>();
+		al.add(1);
+		al.add(3);
+		al.add(3);
+		Set<Integer> set1=new LinkedHashSet<>(al);
+		Set<Integer> set=new LinkedHashSet<>(arr1.length);
+		for (int i = 0; i < arr1.length; i++) {
+			set.add(arr1[i]);
+		}
+		for(int e:set)
+		{
+			System.err.println("set is:"+e);
+		}
+		
+		
+		
+		Set<Integer> set2=new LinkedHashSet<>(al);
+		for(int e:set2)
+		{
+			System.err.print(e);
+		}
+		
+		
+	}
+
+}
